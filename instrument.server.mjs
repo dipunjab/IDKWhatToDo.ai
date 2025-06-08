a5f2fbb11eff3e5688ecfdb74037a0801aad21ec
Usman <ghaniusman0287@gmail.com>
2025-06-08 12:39:43 +0500

    Setup Sentry for Tracking and monitoring Errors

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: "https://81128a91577a44c81ab0e17171e0e8ad@o4509230352891904.ingest.de.sentry.io/4509342557077584",
+  
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  profilesSampleRate: 1.0, // profile every transaction
+});
